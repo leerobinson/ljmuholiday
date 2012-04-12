d20bde6e950eeb7f4b3b82ba3265d9d1a6190ba0
leerobinson <l.robinson1@2009.ljmu.ac.uk>
2012-04-12 18:23:06 +0100

    sorting some issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,22 +2,15 @@ require 'faker'
 require 'populator'
 require 'sha1'
 
-10.times do
-  user = User.new
-  user.username = Faker::Internet.user_name
-  user.email = Faker::Internet.email
-  user.password = "password"
-  user.password_confirmation = "password"
-  user.save
-end
+Holiday.destroy_all
+
 
 User.all.each do |user|
- Holiday.populate(5..10) do |holiday|
-  holiday.amountofdays = Faker::Lorem.sentence
+ Holiday.populate(1) do |holiday|
+  holiday.amountofdays = Integer(8)
   holiday.startdate = DateTime.now
   holiday.enddate = DateTime.now + 8
   holiday.user_id = user.id
  end
 end
-
 	
