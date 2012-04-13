25e7303b57e5ebdbb4ddc320b1afb0acfd71c19c
leerobinson <l.robinson1@2009.ljmu.ac.uk>
2012-04-13 14:29:52 +0100

    readme changed

--- test/unit/holiday_test.rb
+++ test/unit/holiday_test.rb
@@ -1,5 +1,15 @@
-require 'test_helper'
+require File.dirname(__FILE__) + "/../test_helper"
 
 class HolidayTest < ActiveSupport::TestCase
+
+	def create_holiday(attributes = {})
+	
+		attributes[:startdate] ||= '10/04/2012'
+		attributes[:enddate] ||= '18/04/2012'
+		attributes[:amountofdays] ||= '8'
+		attributes[:user_id] ||= '1'
+		holiday = Holiday.new(attributes)
+		holiday
+	end
   
 end
